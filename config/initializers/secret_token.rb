0b0df0d48ab251204a87a97dc35e25ab04560eb6
U-Beast\Motown <Motown@Beast.(none)>
2011-12-30 15:32:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5480a515f0ffb93e745329e0208c97adcdfaf42c8f5f3f57074561bcc62d7871dbf9fd7a02d74b0d714f4ccc944dead5ce785a6b2750a340bd38bebdb6066d39'
